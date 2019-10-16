7622de51e928745935f08c4f249c2d385289567c
Amgad Malik <amgadmalik@yahoo.com>
2019-10-16 23:40:28 +0000

    Automatically backed up by Learn

---
+++ lib/doctor.rb
@@ -0,0 +1,28 @@
+class Doctor
+  
+  attr_accessor :name 
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name 
+    save
+  end 
+  
+  def save 
+    @@all << self 
+  end 
+  
+  def self.all 
+    @@all 
+  end 
+  
+  def new_appointment(patient,date)
+    appointment.new(patient, date, self)
+  end 
+  
+  def patients
+    Appointment.all.select {|patient| patient.doctor == self}
+  end 
+  
+end 
