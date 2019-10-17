401c788d7dbb85d5aa96ce686641c4a393dd9714
Amgad Malik <amgadmalik@yahoo.com>
2019-10-17 00:02:29 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -22,7 +22,7 @@ class Doctor
   end 
   
   def patients
-    Appointment.all.select {|patient| patient.doctor == self}
+    appointments.collect {|patient| patient.doctor}
   end 
   
   def appointments
